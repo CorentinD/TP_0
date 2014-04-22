6df77072a6e89e30625b2d7aecc281c6e75e8cf9
CorentinD <corentin.detre@gmail.com>
2014-04-22 15:31:11 +0200

    first commit

---
+++ src/main/java/foo/App.java
@@ -0,0 +1,17 @@
+package foo;
+
+/**
+ * Hello world!
+ */
+public final class App {
+
+private App() {
+}
+public static void main(final String[] args) {
+System.out.println("Hello World!");
+}
+public static int nothing(){
+return 1;
+}
+}
+
